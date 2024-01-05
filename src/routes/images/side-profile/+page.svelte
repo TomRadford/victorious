80b05ac256acd78395e75245b2671cc7536e4420
Tom Radford <tom@theradford.com>
2024-01-05 08:49:01 +0200

    top padding

--- src/routes/images/side-profile/+page.svelte
+++ src/routes/images/side-profile/+page.svelte
@@ -1,3 +1,3 @@
-<div class="mx-auto max-w-[1000px]" style:--tag="side-profile">
+<div class="mx-auto max-w-[1000px] pt-24" style:--tag="side-profile">
 	<enhanced:img src="/src/lib/assets/images/side-profile.jpeg" class="w-full" />
 </div>
