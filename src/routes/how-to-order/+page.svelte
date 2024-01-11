eb61b01ba513e91bf735cc39a60de8f2f9c7272e
Tom Radford <tom@theradford.com>
2024-01-11 19:27:17 +0200

    add products on how to order page

--- src/routes/how-to-order/+page.svelte
+++ src/routes/how-to-order/+page.svelte
@@ -4,4 +4,7 @@
 
 <div class="mx-auto max-w-2xl">
 	<Instructions />
+	<div class="flex items-center justify-center gap-4 pt-4">
+		<a href="/products" class="btn btn-outline btn-primary"> Products </a>
+	</div>
 </div>
